00779d271db38b9b4ec7f1b0a1d066019ee4c044
YisakH <drm05003@naver.com>
2021-09-16 10:23:42 +0900

    210916 commit

--- 2342/소스.cpp
+++ 2342/소스.cpp
@@ -1,76 +1,68 @@
-#include <cstdio>
 #include <vector>
+#include <cstdio>
 #include <cmath>
 
 using namespace std;
-
+vector<int> inp;
+vector<vector<vector<int>>> dp;
 int main()
 {
-	int cur, next;
-
-	scanf("%d", &cur);
-	int x = 0, y = 0, sum = 0;
-	while (cur != 0) {
-		scanf("%d", &next);
-
-		if (x == 0) {
-			x = cur;
-			sum += 2;
-		}
-		else if (y == 0) {
-			y = cur;
-			sum += 2;
-		}
-		else if (x == cur || y == cur) {
-			sum += 1;
-		}
-		else if (abs(x - y) == 2) { // 양 반대편에 있으면
-			if (x == next) {
-				y = cur;
-				sum += 3;
-			}
-			else {
-				x = cur;
-				sum += 3;
-			}
-		}
-		else {
-			if (x == next) {
-				if (abs(y - cur) == 2) {
-					y = cur;
-					sum += 4;
-				}
-				else {
-					y = cur;
-					sum += 3;
-				}
-			}
-			else if (y == next) {
-				if (abs(x - cur) == 2) {
-					x = cur;
-					sum += 4;
-				}
-				else {
-					x = cur;
-					sum += 3;
-				}
-			}
-			else {
-				if (abs(x - cur) == 2) {
-					y = cur;
-					sum += 3;
-				}
-				else {
-					x = cur;
-					sum += 3;
-				}
-			}
-		}
-
-
-		cur = next;
+	int input;
+	inp.push_back(0);
+	scanf("%d", &input);
+	while (input != 0) {
+		inp.push_back(input);
+		scanf("%d", &input);
 	}
 
-	printf("%d\n", sum);
-	return 0;
+	dp.assign(inp.size()+1, vector<vector<int>>(5, vector<int>(5, -1)));
+
+	dp[0][0][0] = 0;
+	
+	for (int i = 1; i < inp.size(); i++) {
+		int move = inp[i];
+
+		for (int left = 0; left < 5; left++) {
+			for (int right = 0; right < 5; right++) {
+				if (dp[i - 1][left][right] == -1) continue;
+
+				if (left == move || right == move) {
+					dp[i][left][right] = dp[i - 1][left][right] + 1;
+				}
+				else {
+					int next_left_cost;
+					if (left == 0)
+						next_left_cost = 2;
+					else if (abs(move - left) == 2)
+						next_left_cost = 4;
+					else
+						next_left_cost = 3;
+
+					if (dp[i][move][right] == -1 || dp[i][move][right] > dp[i - 1][left][right] + next_left_cost)
+						dp[i][move][right] = dp[i - 1][left][right] + next_left_cost;
+
+					if (right == 0)
+						next_left_cost = 2;
+					else if (abs(move - right) == 2)
+						next_left_cost = 4;
+					else
+						next_left_cost = 3;
+
+					if (dp[i][left][move] == -1 || dp[i][left][move] > dp[i - 1][left][right] + next_left_cost)
+						dp[i][left][move] = dp[i - 1][left][right] + next_left_cost;
+
+				}
+			}
+		}
+	}
+
+	int minval = 1000000000;
+
+	for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 5; j++) {
+			if (dp[inp.size()-1][i][j] != -1 && dp[inp.size()-1][i][j] < minval)
+				minval = dp[inp.size()-1][i][j];
+		}
+
+	printf("%d", minval);
 }
