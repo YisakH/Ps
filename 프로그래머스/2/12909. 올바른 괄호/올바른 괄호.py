f5678c15874a00d87fbcbf5dc69dc6998ad1164b
YisakH <72448115+YisakH@users.noreply.github.com>
2024-10-08 17:55:37 +0900

    [level 2] Title: 올바른 괄호, Time: 7.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,16 @@
+def solution(s):    
+    cnt = 0
+    
+    for c in s:
+        if c == '(':
+            cnt += 1
+        else:
+            cnt -= 1
+            
+        if cnt < 0:
+            return False
+    
+    if cnt == 0:
+        return True
+    
+    return False
