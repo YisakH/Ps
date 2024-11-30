5048e372e9c34d286506aa8ce27ae7a8e768d054
YisakH <72448115+YisakH@users.noreply.github.com>
2024-11-30 21:46:47 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 1087.37 ms, Memory: 17.6 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,27 +1,21 @@
 def solution(cap, n, deliveries, pickups):
     answer = 0
-    
     for i in range(n-2, -1, -1):
         deliveries[i] += deliveries[i+1]
         pickups[i] += pickups[i+1]
     
     deidx, piidx = n-1, n-1
-    while deidx >= 0 and deliveries[deidx] == 0:
-        deidx -= 1
-    while piidx >= 0 and pickups[piidx] == 0:
-        piidx -= 1
-        
     total_delivered = 0
     total_pickuped = 0
     
     while deidx >= 0 or piidx >= 0:
-        answer += max(deidx + 1, piidx + 1) * 2
-        total_delivered += cap
-        total_pickuped += cap
-        
         while deidx >= 0 and deliveries[deidx] <= total_delivered:
             deidx -= 1
         while piidx >= 0 and pickups[piidx] <= total_pickuped:
-            piidx -= 1       
+            piidx -= 1
+            
+        answer += max(deidx + 1, piidx + 1) * 2
+        total_delivered += cap
+        total_pickuped += cap
         
     return answer
