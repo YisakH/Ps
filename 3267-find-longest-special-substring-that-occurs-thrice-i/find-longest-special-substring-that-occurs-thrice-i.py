ef5431ee87573094e34302e44f743f9997cc0760
YisakH <72448115+YisakH@users.noreply.github.com>
2024-12-10 15:07:32 +0900

    Time: 34 ms (22.70%) | Memory: 17.7 MB (5.52%) - LeetSync

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        answer = 0
+        found = set()
+        i = 0
+        while i < len(s):
+            j = i
+            while j < len(s):
+                if s[j] != s[i]:
+                    break
+                if s[i:j+1] in found:
+                    j += 1
+                    continue
+                print(i, j)
+                found.add(s[i:j+1])
+                cnt = 0
+                findIdx = i
+                while True:
+                    findIdx = s.find(s[i:j+1], findIdx)
+                    if findIdx < 0:
+                        break
+                    findIdx += 1
+                    cnt += 1
+                    if cnt >= 3:
+                        break
+                if cnt >= 3:
+                    answer = max(answer, j - i + 1)
+                    print(s[i:j+1])
+                j += 1
+            i = max(j, i + 1)
+                
+        return answer if answer else -1
