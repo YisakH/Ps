7f53ff76a25e946d1b43a4469a693761a3819efe
YisakH <72448115+YisakH@users.noreply.github.com>
2024-12-18 17:31:13 +0900

    Time: 4 ms (21.42%) | Memory: 18 MB (10.22%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -1,12 +1,16 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        for i in range(len(prices)):
-            for j in range(i + 1, len(prices)):
-                if prices[j] <= prices[i]:
-                    prices[i] -= prices[j]
-                    break
+        st = []
+        answer = prices.copy()
 
-        return prices
+        for i in reversed(range(len(prices))):
+            while st and prices[i] < st[-1]:
+                st.pop()
+            if st:
+                answer[i] -= st[-1]
+            st.append(prices[i])
+
+        return answer
 
 
         
