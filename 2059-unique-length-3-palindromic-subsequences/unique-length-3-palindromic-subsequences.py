09fcda7c0d2d58a4935bf1532e539a3b8fb528b9
YisakH <72448115+YisakH@users.noreply.github.com>
2025-01-04 11:19:07 +0900

    Time: 472 ms (46.67%) | Memory: 17.7 MB (8.33%) - LeetSync

---
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+class Solution(object):
+    def countPalindromicSubsequence(self, s):
+        dic = defaultdict(list)
+        answer = 0
+
+        for i, ch in enumerate(s):
+            dic[ch].append(i)
+        
+        for key, val in dic.items():
+            if len(val) > 1 and val[-1] - val[0] >= 2:
+                myset = set([ch for ch in s[val[0] + 1: val[-1]]])
+                answer += len(myset)
+                print(val[0], val[1])
+        
+        return answer
