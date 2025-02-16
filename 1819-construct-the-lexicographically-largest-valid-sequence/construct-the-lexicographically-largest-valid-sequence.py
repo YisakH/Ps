24a3d58236f1d17c9dc4dee83fae5032f01b8eeb
YisakH <72448115+YisakH@users.noreply.github.com>
2025-02-17 01:45:19 +0900

    Time: 11 ms (18.75%) | Memory: 17.9 MB (21.88%) - LeetSync

--- 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.py
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.py
@@ -2,32 +2,30 @@ class Solution:
     def constructDistancedSequence(self, n: int) -> List[int]:
         answer = [0 for _ in range(2*n-1)]
         
-        used = set() 
-        st = [(0, answer)]
+        def dfs(idx, visited):
+            if idx == 2 * n - 1:
+                return answer
 
-        while st:
-            curIdx, curanswer = st.pop()
-            if curIdx == n * 2 - 1:
-                return curanswer
-            
-            if curanswer[curIdx] != 0:
-                st.append((curIdx + 1, curanswer.copy()))
-                continue
-            
-            if 1 not in curanswer:
-                curanswer[curIdx] = 1
-                st.append((curIdx + 1, curanswer.copy()))
-                curanswer[curIdx] = 0
-            
-            for i in range(2, n+1, 1):
-                if i not in curanswer and curIdx + i < 2 * n - 1 and curanswer[curIdx + i] == 0:
-                    curanswer[curIdx] = i
-                    if i != 1:
-                        curanswer[curIdx + i] = i
+            if answer[idx] != 0:
+                return dfs(idx + 1, visited)
 
-                    st.append((curIdx + 1, curanswer.copy()))
-                    
-                    curanswer[curIdx] = 0
-                    
-                    if i != 1:
-                        curanswer[curIdx + i] = 0
+            for i in range(n, 1, -1):
+                if answer[idx] == 0 and idx + i < 2 * n - 1 and answer[idx + i] == 0 and i not in visited:
+                    answer[idx] = i
+                    answer[idx + i] = i
+                    result = dfs(idx + 1, visited | {(i)})
+                    if result:
+                        return result
+
+                    answer[idx] = 0
+                    answer[idx + i] = 0
+
+            if 1 not in visited:
+                answer[idx] = 1
+                result = dfs(idx + 1, visited | {(1)})
+                if result:
+                    return result  # 정답을 찾으면 즉시 반환
+                answer[idx] = 0
+
+
+        return dfs(0, set())
