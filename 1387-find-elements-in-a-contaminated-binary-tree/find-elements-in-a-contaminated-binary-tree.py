570dc5e1c6b97410a2d77b41dff82be5179c42f2
YisakH <72448115+YisakH@users.noreply.github.com>
2025-02-22 02:18:26 +0900

    Time: 5 ms (74.33%) | Memory: 22.2 MB (66.31%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
@@ -1,12 +1,5 @@
 from typing import Optional
 
-# TreeNode 클래스 정의
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 class FindElements:
     def __init__(self, root: Optional[TreeNode]):
         self.values = set()
