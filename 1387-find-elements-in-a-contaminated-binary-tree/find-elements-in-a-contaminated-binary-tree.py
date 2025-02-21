eab2f2faa3aa129e664d506df39d0cd118b4275a
YisakH <72448115+YisakH@users.noreply.github.com>
2025-02-22 02:17:02 +0900

    Time: 13 ms (25.67%) | Memory: 22.3 MB (37.43%) - LeetSync

---
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+# TreeNode 클래스 정의
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class FindElements:
+    def __init__(self, root: Optional[TreeNode]):
+        self.values = set()
+        self.recover_tree(root, 0)
+
+    def recover_tree(self, node: Optional[TreeNode], value: int):
+        """ 오염된 트리를 복구하고 값들을 저장 """
+        if not node:
+            return
+        node.val = value  # 현재 노드 값 복원
+        self.values.add(value)  # 복원된 값을 set에 저장
+        self.recover_tree(node.left, 2 * value + 1)  # 왼쪽 자식 복구
+        self.recover_tree(node.right, 2 * value + 2)  # 오른쪽 자식 복구
+
+    def find(self, target: int) -> bool:
+        """ target 값이 존재하는지 확인 """
+        return target in self.values
