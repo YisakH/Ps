bd26086fba2330032772c865e269f2d0fc4eab52
YisakH <72448115+YisakH@users.noreply.github.com>
2025-02-25 18:06:08 +0900

    Time: 173 ms (10.48%) | Memory: 27.6 MB (5.11%) - LeetSync

---
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numOfSubarrays(self, arr: List[int]) -> int:
+        # 홀수와 인덱스를 센다
+        acc_sum = arr.copy()
+
+        for i in range(1, len(acc_sum)):
+            acc_sum[i] += acc_sum[i-1]
+
+        print(acc_sum)
+        o_cnt, e_cnt = 0, 0
+        answer = 0
+
+        for i, val in enumerate(acc_sum):
+            if val % 2:
+                answer = (answer + (1 + e_cnt)) % (10**9 + 7)
+                o_cnt += 1
+            else:
+                answer = (answer + o_cnt) % (10**9 + 7)
+                e_cnt += 1
+
+        return answer
+
+        
