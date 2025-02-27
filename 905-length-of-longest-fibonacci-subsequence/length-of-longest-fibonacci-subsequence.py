da654e37ce182422876bea689178ddf98d761153
YisakH <72448115+YisakH@users.noreply.github.com>
2025-02-27 15:25:17 +0900

    Time: 664 ms (87.75%) | Memory: 19.4 MB (36.09%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.py
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.py
@@ -4,13 +4,18 @@ class Solution:
         answer = 0
 
         for i, num in enumerate(arr):
+
             for j in range(i):
-                dp[num].append((arr[j], 2))
+                if num + arr[j] in dp:
+                    dp[num].append((arr[j], 2))
 
             for before, length in dp[num]:
                 answer = max(answer, length)
                 if num + before not in dp:
                     continue
                 dp[num + before].append((num, length + 1))
+
+        #for k, v in dp.items():
+        #    print(k, v)
             
         return answer if answer >= 3 else 0
