530bb9b0a171196195bbd765cf5bfbc458276579
YisakH <72448115+YisakH@users.noreply.github.com>
2025-03-13 16:31:12 +0900

    Time: 215 ms (69.66%) | Memory: 63.1 MB (86.29%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
@@ -4,7 +4,10 @@ class Solution:
         k, i = 0, 0
 
         while i < len(nums):
-            while nums[i] + summ[i] > 0 and k < len(queries):
+            while nums[i] + summ[i] > 0:
+                if k == len(queries):
+                    return -1
+
                 l, r, val = queries[k]
                 l = max(l, i)
 
@@ -20,12 +23,5 @@ class Solution:
                 summ[i + 1] += summ[i]
             
             i += 1
-
-        if k < len(queries):
-            return k
-
-        for i in range(len(nums)):
-            if nums[i] + summ[i] > 0:
-                return -1
             
         return k
