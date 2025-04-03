a96fb6057ed8a5e83e192cd9e50e90fa7a5ab043
YisakH <72448115+YisakH@users.noreply.github.com>
2025-04-03 18:09:55 +0900

    Time: 107 ms (49.39%) | Memory: 60.9 MB (64.23%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -2,13 +2,17 @@ from collections import defaultdict
 
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        dp = defaultdict(int)
+        N = len(questions)
+        dp = [0 for _ in range(N + 1)]
         answer = 0
 
         for i, (point, bp) in enumerate(questions):
-            dp[i + bp + 1] = max(dp[i + bp + 1], dp[i] + point)
+            if i + bp < N:
+                dp[i + bp + 1] = max(dp[i + bp + 1], dp[i] + point)
+            else:
+                dp[N] = max(dp[i] + point, dp[N])
+            
             dp[i+1] = max(dp[i], dp[i+1])
 
-            answer = max(answer, dp[i + bp + 1])
-
-        return answer
+        #print(dp)
+        return max(dp)
