d6999ddcc9ecede8ce7a53b466adeeaced6779ae
YisakH <72448115+YisakH@users.noreply.github.com>
2025-04-08 14:07:30 +0900

    Time: 0 ms (100.00%) | Memory: 17.8 MB (73.67%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -2,20 +2,12 @@ from collections import Counter
 
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        counter = Counter(nums)
+        visit = set()
 
-        dupli = 0
-
-        for val in counter.values():
-            if val > 1:
-                dupli += 1
-        
-        if dupli == 0:
-            return 0
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] in visit:
+                return i // 3 + 1
             
-        for i, num in enumerate(nums):
-            counter[num] -= 1
-            if counter[num] == 1:
-                dupli -= 1
-                if dupli == 0:
-                    return i // 3 + 1
+            visit.add(nums[i])
+        
+        return 0
