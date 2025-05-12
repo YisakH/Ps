835d261b540af56333bfcdf459a943ad2ac31a3a
YisakH <72448115+YisakH@users.noreply.github.com>
2025-05-12 16:00:02 +0900

    Time: 1807 ms (41.49%) | Memory: 17.8 MB (65.57%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
@@ -1,43 +1,12 @@
-from collections import Counter
+from itertools import permutations
 
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
-        count = Counter(digits)
-        start, mid, end = [], [], []
-        
-        for key, val in count.items():
-            val = min(val, 3)
-            count[key] = val
+        nums = list({
+            a * 100 + b * 10 + c
+            for a, b, c in permutations(digits, 3)
+            if a != 0 and c % 2 != 1
+        })
 
-            mid.append(key)
-
-            if key == 0:
-                end.append(key)
-            elif key % 2 == 0:
-                start.append(key)
-                end.append(key)
-            else:
-                start.append(key)
-        
-        answer = []
-        
-        for e in end:
-            count[e] -= 1
-
-            for s in start:
-                if count[s] == 0:
-                    continue
-                count[s] -= 1
-
-                for m in mid:
-                    if count[m] == 0:
-                        continue
-                    
-                    answer.append(int(str(s) + str(m) + str(e)))
-
-                count[s] += 1
-
-            count[e] += 1
-
-        answer.sort()
-        return answer
+        nums.sort()
+        return nums
