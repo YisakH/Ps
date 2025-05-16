5ae0b4aab1202e20d9c959ee818d0dc12f1513db
YisakH <72448115+YisakH@users.noreply.github.com>
2025-05-16 16:56:51 +0900

    Time: 1700 ms (20.45%) | Memory: 18.3 MB (62.50%) - LeetSync

--- 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.py
+++ 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.py
@@ -3,16 +3,7 @@ from collections import defaultdict
 class Solution:
     def getWordsInLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
         def checkDiff(word1, word2):
-            if len(word1) != len(word2):
-                return -1
-        
-            res = 0
-            
-            for ch1, ch2 in zip(word1, word2):
-                if ch1 != ch2:
-                    res += 1
-            
-            return res
+            return len(word1) == len(word2) and sum(ch1 != ch2 for ch1, ch2 in zip(word1, word2)) == 1
 
         N = len(words)
         dp = [[1, -1] for _ in range(N)]
@@ -23,16 +14,11 @@ class Solution:
                     if dp[j][0] < dp[i][0] + 1:
                         dp[j] = [dp[i][0] + 1, i]
         
-        maxidx = 0
+        maxidx = max(range(N), key = lambda x: dp[x][0])
         ans = []
-
-        for i in range(N):
-            if dp[maxidx][0] < dp[i][0]:
-                maxidx = i
 
         while maxidx != -1:
             ans.append(words[maxidx])
             maxidx = dp[maxidx][1]
-        ans.reverse()
-        
-        return ans
+
+        return ans[::-1]
