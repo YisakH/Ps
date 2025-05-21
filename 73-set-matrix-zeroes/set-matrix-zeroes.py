c16641c807b5fb197b46a08df42430b3b7409302
YisakH <72448115+YisakH@users.noreply.github.com>
2025-05-21 17:32:15 +0900

    Time: 0 ms (100.00%) | Memory: 18.4 MB (78.41%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,18 +1,30 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        rows = set()
-        columns = set()
+        N, M = len(matrix), len(matrix[0])
+        rows_init = any(matrix[i][0] == 0 for i in range(N))
+        cols_init = any(matrix[0][j] == 0 for j in range(M))
 
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+
+        for i in range(N):
+            for j in range(M):
                 if matrix[i][j] == 0:
-                    rows.add(i)
-                    columns.add(j)
-
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if i in rows or j in columns:
+                    matrix[i][0] = 'c'
+                    matrix[0][j] = 'c'
+        
+        for i in range(1, N):
+            if matrix[i][0] == 'c':
+                for j in range(M):
                     matrix[i][j] = 0
         
-        return matrix
-        
+        for j in range(1, M):
+            if matrix[0][j] == 'c':
+                for i in range(N):
+                    matrix[i][j] = 0
+
+        if rows_init:
+            for i in range(N):
+                matrix[i][0] = 0
+
+        if cols_init:
+            for j in range(M):
+                matrix[0][j] = 0
