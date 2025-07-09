79669b94cbdbb30e38566dc67b7a6f440123356f
YisakH <72448115+YisakH@users.noreply.github.com>
2025-07-09 15:44:57 +0900

    Time: 68 ms (40.30%) | Memory: 36 MB (87.45%) - LeetSync

--- 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.py
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.py
@@ -3,21 +3,23 @@ from collections import deque
 class Solution:
     def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
         # k 개를 옮긴다? 결국 연속 k개 사이에 있는 쉬는시간의 합을 구하라 문제
+        #startTime = [0] + starTime
+        #endTime = [0] + endTime
         ans = 0
-        cur_sum = 0
+        cur_sum = startTime[0]
+        i = 0
 
-
-        q = deque([[0,0]])
-
-        for s, e in zip(startTime, endTime):
-            cur_sum += s - q[-1][1]
+        for j in range(1, len(startTime)):
+            cur_sum += startTime[j] - endTime[j-1]
             ans = max(ans, cur_sum)
-            q.append([s, e])
 
-            if len(q) > k + 1:
-                qs, qe = q.popleft()
-                cur_sum -= q[0][0] - qe
+            if j - i + 1 > k:
+                if i > 0:
+                    cur_sum -= startTime[i] - endTime[i-1]
+                else:
+                    cur_sum -= startTime[i]
+                i += 1
         
-        ans = max(ans, cur_sum + eventTime - q[-1][1])
+        ans = max(ans, cur_sum + eventTime - endTime[-1])
         
         return ans
