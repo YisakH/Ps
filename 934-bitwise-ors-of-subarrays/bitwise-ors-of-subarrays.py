67dc5924fd2c98164d68a5ff1559f5a09c02fd20
YisakH <72448115+YisakH@users.noreply.github.com>
2025-08-01 00:48:32 +0900

    Time: 359 ms (97.62%) | Memory: 45.1 MB (12.50%) - LeetSync

--- 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.py
+++ 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.py
@@ -1,16 +1,16 @@
 class Solution:
     def subarrayBitwiseORs(self, arr):
-        result = set()   # 최종 distinct OR 값 저장
-        prev = set()     # 이전까지의 subarray OR 값 저장
+        result = set()
+        prev = set()
 
         for num in arr:
-            # 현재 num을 추가했을 때 가능한 모든 OR값 갱신
             curr = {num}
+
             for p in prev:
                 curr.add(p | num)
 
             result.update(curr)
-            prev = curr  # 현재 OR 결과를 다음에 넘김
+            prev = curr
 
         return len(result)
 
