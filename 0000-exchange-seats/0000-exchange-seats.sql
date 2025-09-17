f3c9f420ea972668bde651b7672a4c9e8d76ebac
YisakH <72448115+YisakH@users.noreply.github.com>
2025-09-17 18:09:38 +0900

    Time: 1219 ms (5.11%), Space: 0B (100%) - LeetHub

--- 0000-exchange-seats/0000-exchange-seats.sql
+++ 0000-exchange-seats/0000-exchange-seats.sql
@@ -1,21 +1,14 @@
 /* Write your T-SQL query statement below */
 
-select *
-from (
-    select *
-    from seat
-    where id % 2 = 1 and id = ( select max(id) from seat)
+declare @maxval int;
+set @maxval = (select max(id) from seat)
 
-    union
-
-    select id + 1 id, student
-    from seat
-    where id % 2 = 1 and id != ( select max(id) from seat)
-
-    union
-
-    select id - 1 id, student
-    from seat
-    where id % 2 = 0
-) tb
+select
+    case
+        when id % 2 = 1 and id < (@maxval) then id + 1
+        when id % 2 = 0 then id - 1
+        else id
+    end id,
+    student
+from seat
 order by id
