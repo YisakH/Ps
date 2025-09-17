71bedd93c5a810b00a3da2313c76d13738af6ea7
YisakH <72448115+YisakH@users.noreply.github.com>
2025-09-17 16:31:09 +0900

    Time: 653 ms (57.38%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+
+select e1.name
+from employee e1
+    left join employee e2 on e1.id = e2.managerId
+group by e1.id, e1.name
+having count(e1.id) >= 5
