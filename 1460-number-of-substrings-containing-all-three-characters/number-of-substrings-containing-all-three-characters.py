890aa576c1cb2ba0343543d6ad01fe4889c978b8
YisakH <72448115+YisakH@users.noreply.github.com>
2026-06-30 23:35:34 +0900

    Time: 153 ms (46.51%) | Memory: 14.3 MB (23.47%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -1,28 +1,16 @@
-from bisect import bisect_left
-
-class Solution:
-    def numberOfSubstrings(self, s: str) -> int:
-        """
-          카운트를 한다.
-
-        """
-        dic = {'a':[], 'b':[], 'c':[]}
+class Solution(object):
+    def numberOfSubstrings(self, s):
+        N = len(s)
+        d = {'a':0, 'b':0, 'c':0}
+        start = 0
         answer = 0
 
-        for i, ch in enumerate(s):
-            dic[ch].append(i)
+        for end in range(N):
+            d[s[end]] += 1
 
-        for start in range(len(s)):
-            min_idx = 0
-            for ch in ['a', 'b', 'c']:
-                idx = bisect_left(dic[ch], start)
-                if idx >= len(dic[ch]):
-                    min_idx = None
-                    break
-                min_idx = max(min_idx, dic[ch][idx])
-
-            if min_idx:
-                answer += len(s) - min_idx
-            
+            while d['a'] and d['b'] and d['c']:
+                answer += N - end
+                d[s[start]] -= 1
+                start += 1
         
         return answer
